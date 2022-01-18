0248fcdfbf3dbfd72aafad87c3e56aae47f8d85a
yangjing <yangjing@example.com>
2022-01-18 22:18:45 +0800

    209

--- CodeCaprice/code/leecode209.cpp
+++ CodeCaprice/code/leecode209.cpp
@@ -4,52 +4,71 @@ using namespace std;
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        vector<int> lengthList;
+        // 经典滑动窗口，先找到一个不小于target的子数组，通过头指针增加缩小窗口，知道该窗口内的子数组为小于target的子数组（这样就能找到一个满足target的最小窗口），然后尾指针不断增加，直到窗口内的数组重新满足target，重复循环。
         int start = 0;
-        int end = 0;
-        int minLength = 0;
-        int sum = nums[start];
+        int sum = 0;
+        int subLength = 0;
+        int minLength = INT32_MAX;
+        for(int end = 0; end < nums.size(); end++){
+            sum += nums[end];
+            while(sum >= target){
+                subLength = end - start + 1;
+                minLength = subLength < minLength ? subLength : minLength;
+                sum -= nums[start++];
+            }
+        }
+        return minLength == INT32_MAX ? 0 : minLength;
         
-        // if (nums[start] >= target){
-        //     return 1;
+        
+        // int start = 0;
+        // int end = 0;
+        // int minLength = 0;
+        // int sum = nums[start];
+
+        // for(int i = 0; i < nums.size(); i++){
+        //     if(nums[i] >= target){
+        //         return 1;
+        //     }
         // }
-        // end = start + 1;
-        while(end < nums.size()){
-            sum += nums[++end];
-            if(sum >= target){
-                if (end - start + 1 > minLength){
-                    minLength = end - start + 1;
-                    break;
-                }
-            }
-        }
-        if (minLength == 0 || minLength == 1){
-            return minLength;
-        }
-        while(start < end && end < nums.size() - 1){
-            start++;
-            end++;
-            if (sum - nums[start] + nums[end] >= target){
-                while(start < end){
-                    if(sum - nums[--start] >= target){
-                        minLength--;
-                        continue;
-                    }else{
-                        start++;
-                    }
-                }
-            }
-            
-        }
-            
-        
-        return minLength;
+
+        // while(end < nums.size() - 1){
+        //     sum += nums[++end];
+        //     if(sum >= target){
+        //         if (end - start + 1 > minLength){
+        //             minLength = end - start + 1;
+        //         }
+        //         while(start <= end){
+        //             if(sum - nums[start] >= target){
+        //                 sum -= nums[start++];
+        //                 minLength--;
+        //             }else{
+        //                 break;
+        //             }
+        //         }
+        //         break;
+        //     }
+        // }
+        // if (minLength == 0 || minLength == 1){
+        //     return minLength;
+        // }
+        // while(start <= end && end <= nums.size() - 2){
+        //     sum = sum - nums[start++] + nums[++end];
+        //     while (sum >= target && start <= end){
+        //         if(sum - nums[start] >= target){
+        //             sum -= nums[start++];
+        //             minLength--;
+        //         }else{
+        //             break;
+        //         }
+        //     }
+        // }
+        // return minLength;
     }
 };
 int main(){
     Solution su;
-    vector<int> arr = {1, 4, 4};
-    int num = su.minSubArrayLen(4, arr);
+    vector<int> arr = {1,1,1,1,1};
+    int num = su.minSubArrayLen(11, arr);
     cout << num;
     return 0;
 }
