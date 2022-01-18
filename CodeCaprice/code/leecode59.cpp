8f3c6616932c9b8f3b21f9c47692a6779fcd79cc
yangjing <yangjing@example.com>
2022-01-19 00:44:49 +0800

    init 59

---
+++ CodeCaprice/code/leecode59.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<int> tmp(n, 0);
+        vector<vector<int>> vec(n, tmp); 
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j ++){
+
+            }
+        }
+    }
+};
