217db60e0fb65abcae2ec2f69f24f505deff8843
yangjing <yangjing@example.com>
2022-01-21 17:48:52 +0800

    update jianzhi29

--- CodeCaprice/code/jianzhioffer29.cpp
+++ CodeCaprice/code/jianzhioffer29.cpp
@@ -13,11 +13,11 @@ public:
             return {};
         }
         vector<int> res(row*col, 0);
+        int cnt = 0;
         int startx=0;
         int starty = 0;
         int loop = col/2;
         int offset = 0;
-        int cnt = 0;
         while(loop--){
             int i,j;
             for(j = starty; j < starty + col - offset - 1; j++){
@@ -39,6 +39,18 @@ public:
         if(row == col && row%2 != 0){
             res[cnt] = matrix[row/2][row/2];
         }
+        if(row != col ){
+            for(int i = 0; i < col; i++){
+                res[cnt++] = matrix[0][i];
+            }
+            return res;
+        }
+        if(col != col){
+            for(int i = 0; i < row; i++){
+                res[cnt++] = matrix[i][0];
+            }
+            return res;
+        }
         return res;
     }
 
@@ -54,7 +66,7 @@ int main(){
     Solution su;
     // vector<int> num = {5,7,7,8,8,10};
     // vector<int> array = su.searchRange(num, 8);
-    vector<vector<int>> matrix_ = {{3},{2}};
+    vector<vector<int>> matrix_ = {{2,3,4},{5,6,7},{8,9,10},{11,12,13}};
     vector<int> array = su.spiralOrder(matrix_);
     su.printNums(array);
     return 0;
