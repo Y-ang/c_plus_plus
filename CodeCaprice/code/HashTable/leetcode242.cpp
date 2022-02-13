326d3d888a53d7e6521ffabb5bcf8b9fa8c03ca5
yangjing <yangjing@example.com>
2022-02-13 21:14:45 +0800

    有效的字母异位词

--- CodeCaprice/code/HashTable/leetcode242.cpp
+++ CodeCaprice/code/HashTable/leetcode242.cpp
@@ -1,3 +1,15 @@
+/*
+242. 有效的字母异位词
+给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+
+注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/valid-anagram
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+// 使用stl map
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -31,3 +43,5 @@ public:
 
     }
 };
+
+// 使用一维数组
