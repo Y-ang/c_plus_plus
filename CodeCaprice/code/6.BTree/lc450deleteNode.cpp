ba60c4aa4e6974ccccef3166d5af8b8364833f7f
yangjing <yangjing@example.com>
2022-03-07 22:17:09 +0800

    update

--- CodeCaprice/code/6.BTree/lc450deleteNode.cpp
+++ CodeCaprice/code/6.BTree/lc450deleteNode.cpp
@@ -1,14 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode* l, TreeNode* r) : val(x), left(l), right(r) {}
-};
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -24,48 +13,47 @@ class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
         TreeNode* cur = root;
+        TreeNode* parent = nullptr;
         while (cur != nullptr) {
             if (key == cur->val){
-                replaceNode(cur);
                 break;
-            } else if (key < cur->val) {
+            } 
+            parent = cur;
+            if (key < cur->val) {
                 cur = cur->left;
             } else {
                 cur = cur->right;
             }
         }
+        // 删除头节点
+        if (parent == nullptr) {
+            return deleteCurNode(cur);
+        }
+        if (parent->left && parent->left->val == key) {
+            parent->left = deleteCurNode(cur);
+        } else if (parent->right && parent->right->val == key) {
+            parent->right = deleteCurNode(cur);
+        }
+        delete cur;
         return root;
     }
-    void replaceNode(TreeNode* node) {
-        TreeNode* tmp = node;
+    TreeNode* deleteCurNode(TreeNode* node) {
+        if (node == nullptr) return nullptr;
         if (!node->left && node->right) {
-            node = node->right;
+            return node->right;
         } else if (node->left && !node->right) {
-            node = node->left;
+            return node->left;
         } else if (node->left && node->right) {
             TreeNode* leave = node->right;
             while (leave->left != nullptr) {
                 leave = leave->left; 
             }
             leave->left = node->left;
-            node = node->right;
+            return node->right;
             // delete tmp;
-
         }
-        // delete tmp;
-        return;
-    }
-};
+        return nullptr;
 
- int main() {
-    Solution su;
-    TreeNode* leave1 = new TreeNode(2);
-    TreeNode* leave2 = new TreeNode(4);
-    TreeNode* leave4 = new TreeNode(7);
-    TreeNode* nodeL = new TreeNode(3, leave1, leave2);
-    TreeNode* nodeR = new TreeNode(6, nullptr, leave4);
-    TreeNode* root = new TreeNode(5, nodeL, nodeR);
-    int key = 3;
-    su.deleteNode(root, key);
-    return 0;
- }
+    }
+
+};
