3c60bf9390430727e4bd33376fef6a3d9c05685d
yangjing <yangjing@example.com>
2022-03-18 11:54:55 +0800

    update

--- CodeCaprice/code/8.Greedy/lc435eraseOverlapIntervals.cpp
+++ CodeCaprice/code/8.Greedy/lc435eraseOverlapIntervals.cpp
@@ -11,12 +11,12 @@ public:
         int cnt = 0;
         int preMax = intervals[0][1];
         for (int i = 1; i < intervals.size(); i++) {
-            if (intervals[i][0] < preMax) {
+            if (intervals[i][0] < preMax) { // 有重叠
                 cnt++;
-                continue;
+                preMax = min(preMax, intervals[i][1]); // 删掉范围大的，保留范围小的
             }
-            
         }
+        return cnt;
     }
 };
 
